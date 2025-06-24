3c81312d5143bd2ba10317fae715be4f4dfb24a7
Anurag0577 <anurag0577@gmail.com>
2025-06-24 15:09:25 +0530

    Connect to mongoose db.

---
+++ Backend/src/db/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+ const connectDB = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`Mongoose database connected! Host: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log('Database connection failed!', error.message)
+        process.exit(1); // terminate the program or process
+        // This approach is not good, we should try to reconnect with db few times before exit. Since you are beginner, this is okay.
+    }
+ }
+
+ export default connectDB;
