11d5dd5b651c5b7e452143084e1c841547384b77
Anurag0577 <anurag0577@gmail.com>
2025-06-24 15:14:11 +0530

    Wrote code to manage api responses and async api errors

---
+++ Backend/src/utiles/apiResponse.js
@@ -0,0 +1,15 @@
+class apiResponse{
+    constructor(
+        statusCode,
+        message = "Success",
+        data
+    ){
+        super(message);
+        this.statusCode = statusCode,
+        this.message = message,
+        this.data = data,
+        this.success = statusCode >= 200 && statusCode < 300;
+    }
+}
+
+export default apiResponse;
