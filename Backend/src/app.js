213c42862f254242f90b4be326fcfba3601f2064
Anurag0577 <anurag0577@gmail.com>
2025-06-30 18:50:59 +0530

    Fix some issues.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,14 +2,28 @@ import express from "express";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import cors from 'cors';
+import connectDB from "./db/index.js";
 
 const app = express();
+const PORT = 3000;
 
-app.use(cors);
+app.use(cors());
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    console.log('Welcome to the Youtube clone!')
+    res.send('Welcome to the YouTube Clone.')
+})
 app.use('/file', uploadRoutes);
-app.use('/authentication', authRoutes );
+app.use('/auth', authRoutes );
 
 
-export default app;
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server started successfully! Running on port ${PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed! ", error);
+})
