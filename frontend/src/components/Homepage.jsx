05a051f71aafdc07b35060780617958a54ac3fb0
Anurag Singh <anurag0577@gmail.com>
2025-09-01 00:53:47 +0530

    build sidebar on homepage

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import Header from "./Header";
+import { useSelector } from "react-redux";
 import HomepageGrid from "./HomepageGrid.jsx";
+import { AiOutlineHome } from 'react-icons/ai';
+import { MdOutlineSubscriptions } from 'react-icons/md';
 import { BsCameraVideoOffFill } from 'react-icons/bs';
+import { FiTrendingUp } from 'react-icons/fi';
 function Homepage() {
     // Sample video data (you can replace this with your actual data)
     const [randomVideos, setRandomVideos] = useState([]);
     const user = JSON.parse(localStorage.getItem('user') || 'null');
+    const isSidebarOpen = useSelector((state) => state.sidebarHandler.value);
+    console.log("isSidebarOpen", isSidebarOpen)
 
 
     useEffect(() => {
@@ -31,24 +37,67 @@ function Homepage() {
         
     }, [])
 
-    // const videos = Array(9).fill({
-    //     thumbnail: "https://res.cloudinary.com/dywh2ogcw/image/upload/v1754424554/images/lobft8my2615fbyiod7w.jpg",
-    //     title: "Toji Top 10 Best Badass scenes | Jujutsu Kaisen | Anime Hindi",
-    //     channel: "Viper Anime Explained",
-    //     views: "1M views · 1 year ago",
-    //     channelAvatar: "https://res.cloudinary.com/dywh2ogcw/image/upload/v1754424554/images/lobft8my2615fbyiod7w.jpg"
-    // });
-
-    // <HomepageGrid 
-//     videos={randomVideos}
-//     currentUserAvatar={user?.avatar}
-// />
 
     return (
         <>
             <Header />
             
-            { (randomVideos.length > 0) ? <HomepageGrid videos={randomVideos} currentUserAvatar={user?.avatar} /> : <div className=" w-full h-screen flex justify-center mt-7 bolder text-2xl" >Sorry, No video available!</div> }
+
+            <div className="flex w-full h-full mt-16">
+                {isSidebarOpen ? (
+                          <div className="sideBar-container fixed min-w-[200px] h-[100vh] text-black bg-white flex flex-col">
+                          {/* Sidebar Items */}
+                          <div className="sidebar-item-container flex-1">
+                            <ul className="space-y-2 p-4">
+                              <li className="px-4 py-2 rounded flex hover:bg-black hover:text-white cursor-pointer transition">
+                                <AiOutlineHome className="text-2xl mr-2"/>
+                                Home
+                              </li>
+                              <li className="px-4 py-2 rounded flex hover:bg-black hover:text-white cursor-pointer transition">
+                                <FiTrendingUp className="text-2xl mr-2"/>
+                                Trending Videos
+                              </li>
+                              <li className="px-4 py-2 rounded flex hover:bg-black hover:text-white cursor-pointer transition">
+                                <MdOutlineSubscriptions className="text-2xl mr-2"/>
+                                Subscription
+                              </li>
+                            </ul>
+                          </div>
+                
+                          {/* Footer (optional) */}
+                          <div className="p-4 border-t border-gray-700 text-xs text-gray-400">
+                            © 2025 My App
+                          </div>
+                        </div>) 
+                        :
+                        (<div className="sideBar-container fixed min-w-[50px] h-[100vh] text-black bg-white flex flex-col  ">
+                          {/* Sidebar Items */}
+                          <div className="sidebar-item-container flex-1">
+                            <ul className="space-y-2 p-4">
+                              <li className="px-4 py-2 rounded flex hover:bg-black hover:text-white cursor-pointer transition">
+                                <AiOutlineHome className="text-2xl "/>
+                              </li>
+                              <li className="px-4 py-2 rounded flex hover:bg-black hover:text-white cursor-pointer transition">
+                                <FiTrendingUp className="text-2xl"/>
+                              </li>
+                              <li className="px-4 py-2 rounded flex hover:bg-black hover:text-white cursor-pointer transition">
+                                <MdOutlineSubscriptions className="text-2xl "/>
+                              </li>
+                            </ul>
+                          </div>
+                
+                        </div>)
+                
+                        }
+
+                        {/* homepage grid*/}
+                        
+                        <div className={`flex-1 ${isSidebarOpen ? 'ml-[200px]' : 'ml-[50px]'} min-h-screen transition-all duration-300`}>
+                        { (randomVideos.length > 0) ? <HomepageGrid videos={randomVideos} currentUserAvatar={user?.avatar} /> : <div className=" w-full h-screen flex justify-center mt-7 bolder text-2xl" >Sorry, No video available!</div> }
+                        </div>
+            </div>
+            
+            
             
         </>
     );
